8208529a6b16693fbd78d08620c976f834cb63e8
ebonali <ebonali05@gmail.com>
2026-01-27 08:33:35 -0800

    switch to smtp port 587 for reliability

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,8 +4,8 @@ const sendEmail = async (options) => {
     // Create transporter
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST || 'smtp.gmail.com',
-        port: process.env.SMTP_PORT || 465,
-        secure: true, // true for 465, false for other ports
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
